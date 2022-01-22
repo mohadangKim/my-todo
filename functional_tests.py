5325ea4f7aeeb37b75d98c8b75290ddded4d00fc
mohadangKim <ehdlaos@gmail.com>
2022-01-22 20:47:25 +0900

    변경 - 기능 테스트가 작업 아이템을 추가할 수 있는지 확인

--- functional_tests.py
+++ functional_tests.py
@@ -1,18 +1,41 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
-        return super().tearDown()
+        self.browser.quit()
 
     def test_can_start_a_list_and_retriev_it_later(self):
         self.browser.get('http://localhost:8000')
+
+        # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+
+        # "공작깃털 사기"라고 텍스트 상자에 입력
+        inputbox.send_keys('공작깃털 사기')
+
+        # 엔터키를 치면 페이지가 갱신
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
 
 
 if __name__ == '__main__':
