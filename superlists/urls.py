1df58954148c662cbf0941b3559e96da5d4f495b
mohadangKim <ehdlaos@gmail.com>
2022-01-22 19:40:48 +0900

    추가 - home_page 요청 테스트, 기능 추가

--- superlists/urls.py
+++ superlists/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
-    # url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^$', 'lists.views.home_page', name='home'),
 ]
